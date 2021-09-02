89bb4535090a1e3775391116b8145dd78a722199
Harisur-Sojol <harisursojol450@gmail.com>
2021-09-02 01:43:50 -0700

    some changes applied

--- book.js
+++ book.js
@@ -9,18 +9,20 @@ const searchBook = () => {
         errorResult.textContent = '';
         const div = document.createElement('div')
         div.innerHTML = `
-        <h1 class="bg-danger p-4 text-center w-75 mx-auto mt-5">Search field cannot be empty</h1
+        <h3 class="bg-danger p-4 text-center w-75 mx-auto mt-5">Search field cannot be empty</h3>
         `
         errorResult.appendChild(div);
         displayBooks.textContent = ''
     }
     else if(searchText.length > 0){
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
+        console.log(url)
         fetch(url)
             .then(res => res.json())
             .then(data => displayBook(data.docs))
             errorResult.textContent = '';
     }
+    
 }
 const displayBook = (books) => {
     searchInput.value = '';
@@ -32,14 +34,13 @@ const displayBook = (books) => {
         div.classList.add('col')
         div.innerHTML = `
         <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid " alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top book-cover" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Book Name: ${book.title}</h5>
                     <h6 class="card-title">Author Name: ${book.author_name ? book.author_name
                     [0] : 'Unknown Author'}</h6>
-                    <h6 class="card-title">Publish year: ${book.first_publish_year ? book.first_publish_year : 'Unknown publish year'}</h6>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.</p>
+                    <h6 class="card-title">First Publish Year: ${book.first_publish_year ? book.first_publish_year : 'Unknown publish year'}</h6>
+                    <h6 class="card-text">Published By: ${book.publisher}</h6>
                 </div>
         </div>
         `
@@ -47,6 +48,9 @@ const displayBook = (books) => {
     })
     const bookQuantity  = displayBooks.childElementCount;
     if(bookQuantity > 0){
-      quantityResult.innerHTML = `<h1>About ${bookQuantity} Results Found</h1>`
+      quantityResult.innerHTML = `<h3 class = "text-primary">About ${bookQuantity} Results Found</h3>`
+    }
+    else if(bookQuantity === 0){
+        quantityResult.innerHTML = `<h3> No result found</h3>`
     }
 }
