eb1528900827ba005e5c14791cd36b1391b05b06
Harisur-Sojol <harisursojol450@gmail.com>
2021-09-01 22:39:55 -0700

    first commit

--- book.js
+++ book.js
@@ -1,13 +1,15 @@
 const searchInput = document.getElementById('search-input');
 const displayBooks = document.getElementById('display-books');
 const errorResult = document.getElementById('show-result')
-
+const quantityResult = document.getElementById('book-quantity');
 const searchBook = () => {
     const searchText = searchInput.value;
     if(searchText.length === 0){
+        quantityResult.innerHTML = '';
+        errorResult.textContent = '';
         const div = document.createElement('div')
         div.innerHTML = `
-        <h1 class="bg-danger p-4 text-center w-75 mx-auto mt-5">write something</h1
+        <h1 class="bg-danger p-4 text-center w-75 mx-auto mt-5">Search field cannot be empty</h1
         `
         errorResult.appendChild(div);
         displayBooks.textContent = ''
@@ -23,13 +25,14 @@ const searchBook = () => {
 const displayBook = (books) => {
     searchInput.value = '';
     displayBooks.textContent = '';
+    quantityResult.innerHTML = ''
     books.forEach(book => {
         console.log(book)
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
         <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img='fluid' " alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid " alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Book Name: ${book.title}</h5>
                     <h6 class="card-title">Author Name: ${book.author_name ? book.author_name
@@ -42,4 +45,8 @@ const displayBook = (books) => {
         `
         displayBooks.appendChild(div);
     })
+    const bookQuantity  = displayBooks.childElementCount;
+    if(bookQuantity > 0){
+      quantityResult.innerHTML = `<h1>About ${bookQuantity} Results Found</h1>`
+    }
 }
